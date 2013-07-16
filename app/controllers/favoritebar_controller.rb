218b65178927b09047a4a36ea8cf2f85603897f7
Christopher Wright <cmwright33@gmail.com>
2013-07-16 19:00:22 -0400

    started to implement adding a new bar

--- app/controllers/favoritebar_controller.rb
+++ app/controllers/favoritebar_controller.rb
@@ -1,14 +1,13 @@
 class FavoritebarController < ApplicationController
-
+  # /Get This will list all the Favorite Bars
   def index
     @bars = Favoritebar.all
   end
-
+  # /Get This will show an individual bar Saved
   def show
     @bar = Favoritebar.find(params[:id])
   end
 
-  def search
 
   end
 
--- app/controllers/neighborhood_controller.rb
+++ app/controllers/neighborhood_controller.rb
@@ -1,15 +1,22 @@
 class NeighborhoodController < ApplicationController
-
+# /Get This Lists All the Neighborhoods on Manhatten
   def index
 
     @neighborhoods = Neighborhood.all
     # This is for Gmaps which I'll use later
     #@json = Bar.all.to_gmaps4rails
   end
-
+# /Get This shows the Popular Bars in Each Neighborhood
   def show
     @neighborhood = Neighborhood.find(params[:id])
     @bars = @neighborhood.bar_info
   end
 
+  def create
+    @new_fav_bar = FavoriteBar.new
+    @new_fav_bar.name
+    @new_fav_bar.url
+    @new_fav_bar.img_url
+    @new_fav_bar
+  end
 end
